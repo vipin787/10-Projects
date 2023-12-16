9608a0db27a71f217ab3b5f9914570959a67f64e
Vipin-chauhan <vipin.chauhan@thewitslab.com>
2023-12-16 15:52:00 +0530

    nike project

---
+++ project-1/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import HeroSection from "./components/HeroSection";
+import Navigation from "./components/Navigation";
+
+function App() {
+  return (
+    <div>
+     <Navigation />
+     <HeroSection />
+    </div>
+  );
+}
+
+export default App;
---
+++ project-1/src/components/Navigation.jsx
@@ -0,0 +1,19 @@
+const Navigation = () => {
+  return (
+    <nav>
+    <div className="logo">
+      <img src="/images/brand_logo.png" />
+    </div>
+    
+    <ul>
+      <li href="#">Home</li>
+      <li href="#">Location</li>
+      <li href="#">About</li>
+      <li href="#">Contact</li>
+    </ul>
+    <button>Login</button>
+    </nav>
+  )
+}
+
+export default Navigation
